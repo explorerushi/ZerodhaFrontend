ab156ced3df435e3d01516e206d293877a76fdc4
Rushikesh Rane <130738163+explorerushi@users.noreply.github.com>
2025-08-11 22:10:39 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,22 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDZsspacycLcDntxiOZ2iIH8uMdghnX0ZM",
-  authDomain: "zerodha-5f8bf.firebaseapp.com",
-  projectId: "zerodha-5f8bf",
-  storageBucket: "zerodha-5f8bf.firebasestorage.app",
-  messagingSenderId: "70047454431",
-  appId: "1:70047454431:web:cad2fc780c8fc2de4ac56a",
-  measurementId: "G-97D960L7E2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
-
-// Initialize services
 const auth = getAuth(app);
 
-// Export both so they can be used anywhere
 export { app, auth };
