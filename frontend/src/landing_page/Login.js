855095e2b41c87657ac733aeed7384dd3e618e3e
Rushikesh Rane <130738163+explorerushi@users.noreply.github.com>
2025-08-11 20:15:49 +0530

    Update Login.js

--- frontend/src/landing_page/Login.js
+++ frontend/src/landing_page/Login.js
@@ -11,7 +11,7 @@ function Login() {
         <form>
           <div className="mb-3">
             <label htmlFor="userid" className="form-label">Email Id</label>
-            <input type="text" className="form-control" id="userid" placeholder="User ID" />
+            <input type="text" className="form-control" id="Email" placeholder="User ID" />
           </div>
 
           {/* Password with Show/Hide */}
@@ -47,4 +47,5 @@ function Login() {
 }
 
 export default Login;
+
 
